8edebaafa8f5ee7a4e5b30e4478f10db23279520
Amar <amarpratap2208@gmail.com>
2022-03-09 18:34:15 +0530

    Prettify test.py

--- test/test.py
+++ test/test.py
@@ -1,7 +1,14 @@
 import requests
 
-# resp = requests.post("http://localhost:5000/predict", files={'file': open('test/dog.jpg', 'rb')})
-resp = requests.post("https://image-classifier-flask-demo.herokuapp.com/predict", files={'file': open('cat.jpg', 'rb')})
+# resp = requests.post(
+# "http://localhost:5000/predict",
+# files={'file': open('test/dog.jpg', 'rb')}
+# )
+
+resp = requests.post(
+    "https://image-classifier-flask-demo.herokuapp.com/predict",
+    files={'file': open('cat.jpg', 'rb')}
+)
 # https://image-classifier-flask-demo.herokuapp.com/
 
 print(resp.text)
